a863516dc90f6a148ebe7b4ab8a959849bc05506
Arthur Feeney <afeeney@uci.edu>
2024-12-29 11:02:22 -0800

    Tidy up setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mondrian-util-lib',
-    packages=find_packages(include=['mondrian_lib', 'mondrian']),
-    version='0.0.0',
-    description='Library for utilities in Mondrian',
-    author='Arthur Feeney',
+    name="mondrian-util-lib",
+    packages=find_packages(include=["mondrian"]),
+    version="0.0.0",
+    description="Library for utilities in Mondrian",
+    author="Arthur Feeney",
     install_requires=[],
 )
