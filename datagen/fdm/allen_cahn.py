829aa74aadea416c1fbeb26a844415c34c63c3e2
Arthur Feeney <afeeney@uci.edu>
2025-01-12 11:52:43 -0800

    Allen Cahn more control on how dataset is generated.

--- datagen/fdm/allen_cahn.py
+++ datagen/fdm/allen_cahn.py
@@ -1,9 +1,11 @@
+import argparse
 import pde
 import numpy as np
 import h5py
 import multiprocessing
 import time
 from scipy.ndimage import gaussian_filter
+from scipy.interpolate import interpn
 
 # This is from pylians: https://pylians3.readthedocs.io/en/master/gaussian_fields.html
 import density_field_library as DFL
@@ -38,36 +40,52 @@ def allen_cahn_init(grid_res, box_size, power=3, pid=None):
         seed = seed ^ pid
     k = np.arange(1, grid_res + 1).astype(np.float32)
     pk = k**-power
+    
+    # truncate frequencies past 32...
+    pk[32:] = 0
+    
     grf = gaussian_field_2d(grid_res, box_size, k, pk, seed)
     grf = np.clip(grf, a_min=-0.9, a_max=0.9)
-    grf = gaussian_filter(grf, sigma=1)
+    
+    # smooth a lot...
+    grf = gaussian_filter(grf, sigma=5)
     return grf
 
 
 def solve_allen_cahn(xlim, ylim, xres, yres, pid):
     assert isinstance(xlim, int)
     assert isinstance(ylim, int)
-    rows = int(ylim * yres)
-    cols = int(xlim * xres)
-    grid = pde.CartesianGrid(([0, ylim], [0, xlim]), (rows, cols))
-    rng = np.random.default_rng()
+    grid = pde.CartesianGrid(([0, ylim], [0, xlim]), (yres, xres))
 
-    field_dim = max(rows, cols)
+    field_dim = max(yres, xres)
     box_size = max(xlim, ylim)
     init = allen_cahn_init(field_dim, box_size, pid=pid)
-    state_data = init[:rows, :cols]
+    state_data = init[:yres, :xres]
     state = pde.ScalarField(grid, data=state_data)
 
+    rng = np.random.default_rng(seed=int(time.time()) ^ pid)
     diffusivity = rng.uniform(low=5e-4, high=5e-3)
 
+    end_time = 6
     storage = pde.MemoryStorage()
-    tracker = [storage.tracker(2)]
+    tracker = [storage.tracker(end_time)]
     eq = pde.AllenCahnPDE(diffusivity, bc={"derivative": 0})
-    eq.solve(state, t_range=2, dt=1e-4, adaptive=True, tracker=tracker, backend="numpy")
+    eq.solve(state, t_range=end_time, dt=1e-4, adaptive=True, tracker=tracker, backend="numpy")
 
     return diffusivity, storage
 
+def cell_centered_points(xlim, ylim, xres, yres):
+    delta_x = xlim / xres
+    delta_y = ylim / yres
+    x_coords = delta_x * (np.arange(0, xres) + 0.5)
+    y_coords = delta_y * (np.arange(0, yres) + 0.5)
+    return x_coords, y_coords
 
+def grid(xlim, ylim, xres, yres):
+    xcoords = np.linspace(0, xlim, xres)
+    ycoords = np.linspace(0, ylim, yres)
+    return xcoords, ycoords
+    
 class Solver:
     def __init__(self, xlim, ylim, xres, yres):
         self.xlim = xlim
@@ -82,28 +100,48 @@ class Solver:
         solution = np.stack([s.data for s in storage])
         return diffusivity, solution, self.xlim, self.ylim
 
-
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn")
-    domain_sizes = [(1, 1)]
-    resolutions = [(128, 128)]
-    dataset_size = 1000
-    with h5py.File("./allen_cahn.hdf5", "w") as f:
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sim_res', required=True, type=int)
+    parser.add_argument('--down_res', required=True, type=int)
+    parser.add_argument('--num_sims', required=True, type=int)
+    args = parser.parse_args()
+    
+    domain_size = (2, 2)
+    resolution = (args.sim_res, args.sim_res)
+    dataset_size = args.num_sims
+    downsample_resolution = (args.down_res, args.down_res)
+    
+    # py-pde uses a cell-centered grid, but I interpolate to a vertex-based grid.
+    x_coords, y_coords = cell_centered_points(1, 1, resolution[0], resolution[1])
+    down_x_coords, down_y_coords = grid(1, 1, downsample_resolution[0], downsample_resolution[1])
+    
+    #cell_centered_points(1, 1, downsample_resolution[0], downsample_resolution[1])
+    target_coords = np.stack(np.meshgrid(down_x_coords, down_y_coords), axis=-1)
+    
+    def interp(data):
+        interp_data = interpn((x_coords, y_coords), data, target_coords, bounds_error=False, fill_value=None)
+        return interp_data
+    
+    with h5py.File(f"./fix_grid_allen_cahn_{args.num_sims}_{args.down_res}.hdf5", "w") as f:
         zfill_cnt = len(str(dataset_size))
-        for domain_size in domain_sizes:
-            for resolution in resolutions:
-                print(f"{domain_size}")
-                xlim, ylim = domain_size
-                xres, yres = resolution
-                size_group = f.create_group(f"res_{xres}_{yres}")
-                processes = min(30, dataset_size)
-                with multiprocessing.Pool(processes=processes) as pool:
-                    output = pool.map(Solver(xlim, ylim, xres, yres), range(dataset_size))
-                for i, (diff, solution, xlim, ylim) in enumerate(output):
-                    g = size_group.create_group(str(i).zfill(zfill_cnt))
-                    g.create_dataset("solution", data=solution)
-                    g.attrs["diffusivity"] = diff
-                    g.attrs["xlim"] = xlim
-                    g.attrs["ylim"] = ylim
-                    g.attrs["xres"] = xres
-                    g.attrs["yres"] = yres
+        xlim, ylim = domain_size
+        xres, yres = resolution
+        down_x, down_y = downsample_resolution
+        size_group = f.create_group(f"res_{down_x}_{down_y}")
+        processes = min(30, dataset_size)
+        with multiprocessing.Pool(processes=processes) as pool:
+            output = pool.map(Solver(xlim, ylim, xres, yres), range(dataset_size))
+            
+        for i, (diff, solution, xlim, ylim) in enumerate(output):
+            print(solution.shape)
+            print(interp(solution.transpose(1, 2, 0)).shape)
+            g = size_group.create_group(str(i).zfill(zfill_cnt))
+            g.create_dataset("solution", data=interp(solution.transpose(1, 2, 0)).transpose(2, 0, 1))
+            g.attrs["diffusivity"] = diff
+            g.attrs["xlim"] = xlim
+            g.attrs["ylim"] = ylim
+            g.attrs["xres"] = xres
+            g.attrs["yres"] = yres
