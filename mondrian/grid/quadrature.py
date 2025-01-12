093095fd9a39c3c499a881e9223c375538cc6911
Arthur Feeney <afeeney@uci.edu>
2025-01-12 11:51:58 -0800

    Improvements to quadrature.
    Since we assume grids are cell-centered, we have interpolate the boundary value. For trapezoid, it uses two points to linearly interpolate the boundary. For Simpsons, it uses three points to fit a polynomial.
    Both then use the trapezoid rule to fit account for the cell between the interpolated boundary and nearest cell-center.

--- mondrian/grid/quadrature.py
+++ mondrian/grid/quadrature.py
@@ -1,11 +1,75 @@
+import functools
+import logging
 from typing import Tuple
 
 import torch
 from torch import nn
 import einops
 
-from mondrian.constants import REIMANN, TRAPEZOID, SIMPSON_13
+from mondrian.constants import REIMANN, TRAPEZOID, SIMPSON_13, QUADRATURE_METHODS
+from .utility import cell_centered_grid
 
+from scipy import integrate
+
+# This can be overwritten to change the integration method,
+# instead of passing a string around everywhere.
+DEFAULT_QUADRATURE_METHOD = REIMANN
+
+def trapezoid_interior_weights_1d(domain_size, num_points, device):
+    delta = domain_size / num_points
+    w = torch.full((num_points,), delta, device=device)
+    w[[0, -1]] = 0.5 * delta
+    return w
+
+def simpson_interior_weights_1d(domain_size, num_points, device):
+    w = torch.empty(num_points, device=device)
+    if num_points % 2 == 1:
+        # if there is an odd number of grid points, we can use simpsons directly.
+        # [1, 4, 2, 4, 2, ..., 2, 4, 1] / 3
+        w[[0, -1]] = 1 / 3
+        w[1:-1:2] = 4 / 3
+        w[2:-1:2] = 2 / 3
+    else:
+        # If there is an even number, we need to initialize first odd-part with simpsons,
+        # and then have the last partition use the trapezoid rule.
+        # [1 / 3, 4 / 3, 2 / 3, 4 / 3, 2 / 3, ...., 2 / 3, 4 / 3, (1 / 3 + .5), 0.5]
+        w[0] = 1 / 3
+        w[1:-2:2] = 4 / 3
+        w[2:-2:2] = 2 / 3
+        w[-2] = (1 / 3 + 0.5)
+        w[-1] = 0.5
+    delta = domain_size / num_points
+    return delta * w
+    
+def apply_linear_boundary_weights(w, domain_size, num_points):
+    delta = domain_size / num_points
+    boundary_delta = delta / 2
+    # fit line going to left boundary
+    w[0] += (5 / 2) * (boundary_delta / 2)
+    w[1] += -(1 / 2) * (boundary_delta / 2)
+    # fit line going to right boundary
+    w[-2] += -(1 / 2) * (boundary_delta / 2)
+    w[-1] += (5 / 2) * (boundary_delta / 2)
+    return w
+
+def apply_linear_boundary_weights2(w, domain_size, num_points):
+    delta = domain_size / num_points
+    boundary_delta = delta / 2
+    # fit curve going to left boundary
+    w[0] += 3 * (boundary_delta / 2)
+    w[1] += -(3 / 2) * (boundary_delta / 2)
+    w[2] += (1 / 2) * (boundary_delta / 2)
+    # fit curve going to right boundary
+    w[-3] += (1 / 2) * (boundary_delta / 2)
+    w[-2] += -(3 / 2) * (boundary_delta / 2)
+    w[-1] += 3 * (boundary_delta / 2)
+    return w
+
+def set_default_quadrature_method(method: str):
+    global DEFAULT_QUADRATURE_METHOD
+    assert method in QUADRATURE_METHODS
+    logging.info(f'changing DEFAULT_QUADRATURE_METHOD to {method}')
+    DEFAULT_QUADRATURE_METHOD = method
 
 def reimann_quadrature_weights(
     grid_spatial_dimensions: Tuple[int],
@@ -23,13 +87,9 @@ def reimann_quadrature_weights(
     Returns:
       The quadrature weights that can be used with a Reimann sum. (This just divides each entry by the discretization).
     """
-    coords = torch.linspace(
-        0, grid_spatial_dimensions[0], grid_discretization_dimensions[0], device=device
-    )
-    delta = coords[1] - coords[0]
     deltas = torch.tensor(
         [
-            delta
+            grid_axis_spatial / grid_axis_disc
             for (grid_axis_spatial, grid_axis_disc) in zip(
                 grid_spatial_dimensions, grid_discretization_dimensions
             )
@@ -49,28 +109,16 @@ def trapezoid_quadrature_weights(
     dims = len(grid_spatial_dimensions)
 
     weight_sizes = [grid_axis_dim for grid_axis_dim in grid_discretization_dimensions]
-    weights = torch.ones(weight_sizes, device=device, dtype=torch.float64)
+    weights = torch.ones(weight_sizes, device=device)
 
     for dim in range(dims):
-
-        w = torch.ones(
-            grid_discretization_dimensions[dim], device=device, dtype=torch.float64
-        )
-        w[[0, -1]] = 0.5
-
+        w = trapezoid_interior_weights_1d(grid_spatial_dimensions[dim], grid_discretization_dimensions[dim], device=device)
+        w = apply_linear_boundary_weights(w, grid_spatial_dimensions[dim], grid_discretization_dimensions[dim])
         # this makes a tensor of size [1, 1, ..., size, ..., 1, 1]
         # this is so it will be broadcasted along the desired axis of weights.
         size = [1 for _ in range(dims)]
         size[dim] = grid_discretization_dimensions[dim]
-
-        coords = torch.linspace(
-            0, grid_spatial_dimensions[dim], grid_discretization_dimensions[dim]
-        )
-        delta = coords[1] - coords[0]
-
-        # TODO: double check this is right...
-        # delta = grid_spatial_dimensions[dim] / grid_discretization_dimensions[dim]
-        weights *= delta * w.reshape(size)
+        weights *= w.reshape(size)
 
     return weights
 
@@ -86,99 +134,43 @@ def simpsons_13_quadrature_weights(
     weight_sizes = [grid_axis_dim for grid_axis_dim in grid_discretization_dimensions]
     weights = torch.ones(weight_sizes, device=device, dtype=torch.float64)
 
-    for dim in range(dims):
-
-        w = torch.empty(
-            grid_discretization_dimensions[dim], device=device, dtype=torch.float64
-        )
-
-        if grid_discretization_dimensions[dim] % 2 == 1:
-            # if there is an odd number of grid-cells, we can use simpsons directly.
-            # [1, 4, 2, 4, 2, ..., 2, 4, 1]
-            w[[0, -1]] = 1 / 3
-            w[1:-1:2] = 4 / 3
-            w[2:-1:2] = 2 / 3
-        else:
-            # If there is an even number, we need to initialize first odd-part with simpsons,
-            # and then have the last partition use the trapezoid rule.
-            # [1, 4, 2, 4, 2, ...., 2, 4, 1.5, 0.5]
-            w[[0, -2]] = 1 / 3
-            w[1:-2:2] = 4 / 3
-            w[2:-2:2] = 2 / 3
-            w[-2] = 1 / 3 + 0.5
-            w[-1] = 0.5
-
+    for dim in range(dims):                
+        w = simpson_interior_weights_1d(grid_spatial_dimensions[dim], grid_discretization_dimensions[dim], device=device)
+        w = apply_linear_boundary_weights2(w, grid_spatial_dimensions[dim], grid_discretization_dimensions[dim])
         # this makes a tensor of size [1, 1, ..., size, ..., 1, 1]
         # this is so it will be broadcasted along the desired axis of weights.
         size = [1 for _ in range(dims)]
         size[dim] = grid_discretization_dimensions[dim]
 
-        coords = torch.linspace(
-            0, grid_spatial_dimensions[dim], grid_discretization_dimensions[dim]
-        )
-        delta = coords[1] - coords[0]
+        weights *= w.reshape(size)
 
-        weights *= delta * w.reshape(size)
+    return weights.to(torch.float32)
 
-    return weights
+#@functools.lru_cache(maxsize=10)
+def get_quadrature_weights(grid_spatial_dimensions: Tuple[int],
+                           grid_discretization_dimensions: Tuple[int],
+                           device: torch.device = None):
+    assert DEFAULT_QUADRATURE_METHOD in QUADRATURE_METHODS
+    if DEFAULT_QUADRATURE_METHOD == REIMANN:
+        quad_func = reimann_quadrature_weights
+    elif DEFAULT_QUADRATURE_METHOD == TRAPEZOID:
+        quad_func = trapezoid_quadrature_weights
+    elif DEFAULT_QUADRATURE_METHOD == SIMPSON_13:
+        quad_func = simpsons_13_quadrature_weights
+    quadrature_weights = quad_func(grid_spatial_dimensions, grid_discretization_dimensions, device=device)
+    return quadrature_weights
 
-
-class Quadrature2d(nn.Module):
-    def __init__(self, method):
-        super().__init__()
-        self.method = method
-        self.cache = {}
-
-        # putting this inside the class, instead of being global makes torch.compile not recompile
-        self._quadrature_lookup = {
-            REIMANN: reimann_quadrature_weights,
-            TRAPEZOID: trapezoid_quadrature_weights,
-            SIMPSON_13: simpsons_13_quadrature_weights,
-        }
-
-    def get_quadrature_weights(
-        self,
-        method: str,
-        grid_spatial_dimensions: Tuple[int],
-        grid_discretization_dimensions: Tuple[int],
-        device: torch.device,
-    ):
-        return self._quadrature_lookup[method](
-            grid_spatial_dimensions, grid_discretization_dimensions, device
-        )
-
-    def forward(self, x):
-        if x.size() in self.cache:
-            return self.cache[x.size()]
-        # This is only used in subdomains, so can use (1, 1)
-        quadrature_weights = self.get_quadrature_weights(
-            self.method, (1, 1), (x.size(-2), x.size(-1)), device=x.device
-        ).to(x.dtype)
-        quadrature_weights.requires_grad_(False)
-        self.cache[x.size()] = quadrature_weights
-        return quadrature_weights
-
-
-class Integral2d(nn.Module):
-    def __init__(self, method):
-        super().__init__()
-        self.method = method
-        self.quadrature = Quadrature2d(method)
-
-    def forward(self, x):
-        if self.method == REIMANN:
-            x = x[..., :-1, :-1]
-        quadrature_weights = self.quadrature(x)
-        return (x * quadrature_weights).sum(dim=[-3, -2, -1])
-
-
-class InnerProduct2d(Integral2d):
-    def __init__(self, method):
-        super().__init__(method)
-
-    def forward(self, x, y):
-        if self.method == REIMANN:
-            x = x[..., :-1, :-1]
-            y = y[..., :-1, :-1]
-        quadrature_weights = self.quadrature(x)
-        return einops.einsum(x * quadrature_weights, y, "... c h w -> ... c h w -> ...")
+#@functools.lru_cache(maxsize=10)
+def get_unit_quadrature_weights(grid_discretization_dimensions: Tuple[int],
+                                device: torch.device = None):
+    assert DEFAULT_QUADRATURE_METHOD in QUADRATURE_METHODS    
+    if DEFAULT_QUADRATURE_METHOD == REIMANN:
+        quad_func = reimann_quadrature_weights
+    elif DEFAULT_QUADRATURE_METHOD == TRAPEZOID:
+        quad_func = trapezoid_quadrature_weights
+    elif DEFAULT_QUADRATURE_METHOD == SIMPSON_13:
+        quad_func = simpsons_13_quadrature_weights
+    grid_spatial_dimensions = tuple([1 for _ in range(len(grid_discretization_dimensions))])
+    quadrature_weights = quad_func(grid_spatial_dimensions, grid_discretization_dimensions, device=device)
+    quadrature_weights.requires_grad_(False)
+    return quadrature_weights
