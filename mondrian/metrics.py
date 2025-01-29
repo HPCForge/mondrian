8ddb5cc301a2eb7d8dbb1e1637cd4b983dd2681c
Arthur Feeney <afeeney@uci.edu>
2025-01-29 10:23:51 -0800

    cleanup metrics

--- mondrian/metrics.py
+++ mondrian/metrics.py
@@ -15,20 +15,10 @@ def max_error(input, target):
     r"""
     Point-wise max across channels and spatial dims.
     This doesn't make sense to use directly for bubbleml,
-    since each field should probably be considered separately.
+    since each variable should probably be considered separately.
     """
     m = torch.max(abs(input - target), dim=[1, 2, 3])
     return torch.mean(m)
-
-def func_l2_error(input, target):
-    height = input.size(-2)
-    width = input.size(-1)
-    quadrature_weights = simpsons_13_quadrature_weights((1, 1), (height, width), device=input.device)
-    
-    # compute quadrature spatially and mean over channels for MSE
-    # then reduce with overall mean.
-    return (quadrature_weights * (input - target)**2).sum(dim=(-2, -1)).mean(-1).mean()
-
 
 class Metrics:
     def __init__(self, log_func):
@@ -40,7 +30,6 @@ class Metrics:
         self.log_func(
             f"{stage}/MaxError",
             max_err.detach(),
-            prog_bar=True,
             batch_size=batch_size,
         )
 
@@ -55,14 +44,6 @@ class Metrics:
         )
 
         l1_err = l1_error(input, target)
-        self.log_func(f"{stage}/L1Error", l1_err.detach(), batch_size=batch_size)
+        self.log_func(f"{stage}/L1Error", l1_err.detach(), batch_size=batch_size, on_step=False, on_epoch=True)
 
-        func_l2_err = func_l2_error(input, target)
-        self.log_func(
-            f"{stage}/FuncL2Error",
-            func_l2_err.detach(),
-            prog_bar=True,
-            batch_size=batch_size
-        )
-
-        return func_l2_err
+        return l2_err
