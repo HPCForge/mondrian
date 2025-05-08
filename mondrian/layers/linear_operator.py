7365bf4c5d5d7840c4b78990a00e16756dafec2e
Arthur Feeney <afeeney@uci.edu>
2025-05-08 12:45:38 -0700

    Remove dropout dropout from linear operators

--- mondrian/layers/linear_operator.py
+++ mondrian/layers/linear_operator.py
@@ -17,30 +17,24 @@ class NeuralOperator(nn.Sequential):
     def __init__(self, in_channels, out_channels, hidden_channels):
         super().__init__(
             LinearOperator2d(in_channels, hidden_channels, bias=True),
-            torch.nn.Dropout(p=0.1),
             nn.GELU(),
             LinearOperator2d(hidden_channels, out_channels, bias=True),
-            torch.nn.Dropout(p=0.1)
         )
 
 class RandomProjectNeuralOperator(nn.Sequential):
     def __init__(self, in_channels, out_channels, hidden_channels, n):
         super().__init__(
             RandomProjectLinearOperator(in_channels, hidden_channels, n, bias=True),
-            torch.nn.Dropout(p=0.1),
             nn.GELU(),
             RandomProjectLinearOperator(hidden_channels, out_channels, n, bias=True),
-            torch.nn.Dropout(p=0.1),
         )
 
 class SeparableRandomProjectNeuralOperator(nn.Sequential):
     def __init__(self, in_channels, out_channels, hidden_channels, n):
         super().__init__(
             SeparableRandomProjectLinearOperator(in_channels, hidden_channels, n, bias=True),
-            torch.nn.Dropout(p=0.1),
             nn.GELU(),
             SeparableRandomProjectLinearOperator(hidden_channels, out_channels, n, bias=True),
-            torch.nn.Dropout(p=0.1),
         )
 
 class LowRankNeuralOperator(nn.Sequential):
@@ -129,7 +123,7 @@ class RandomProjectLinearOperator(LinearOperator2dBase):
         
         self.in_channels = in_channels
         self.n = n
-                
+
         self.params = nn.Parameter(
             torch.randn(
                 self.n,
